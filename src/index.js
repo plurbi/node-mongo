95cabd2f0b3ac9c7e732203263009e4f0621c572
Patricio urbieta <patricio.urbieta@gmail.com>
2018-11-09 22:46:08 -0300

    first deploy

---
+++ src/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+const app = express();
+//connect Db
+mongoose.connect('mongodb://plurbi:plurbi1@ds157493.mlab.com:57493/mdb-node', { useNewUrlParser: true })
+.then(db => console.log('Base conectada'))
+.catch(err => console.log('erro al conectar db', err)
+);
+
+
+
+//settings
+app.set('port', process.env.port || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+
+//middelweres
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended : false }));
+
+//routes
+app.use('/',require('./routes/routes.js'));
+
+//startin server
+app.listen(app.get('port'),() => {
+    console.log('server listen');
+}) 
---
+++ src/routes/routes.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+
+const Task = require('../models/task');
+
+router.get('/', async (req,res) => {
+    const tasks = await Task.find();
+    console.log( 'slash', tasks);
+    res.render('index', { 
+        tasks 
+    });
+});
+
+router.post('/add', async (req,res) => {
+    const task = new Task(req.body);
+    await task.save()
+
+    res.redirect('/');
+});
+
+
+module.exports = router;
