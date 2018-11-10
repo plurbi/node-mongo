52f908d1c1832c454d85b60fd90d2d80aee448eb
Patricio urbieta <patricio.urbieta@gmail.com>
2018-11-10 01:38:52 -0300

    completo funcionamiento

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -13,9 +13,34 @@ router.get('/', async (req,res) => {
 router.post('/add', async (req,res) => {
     const task = new Task(req.body);
     await task.save()
-
     res.redirect('/');
 });
 
+router.get('/turn/:id', async (req,res) => {
+    const { id } = req.params;
+    const task = await Task.findById(id);
+    task.status = !task.status;
+    await task.save();
+    res.redirect('/');
+});
 
+router.get('/delete/:id', async (req,res) => {
+    const { id } = req.params;
+    await Task.remove({ _id: id});
+    res.redirect('/');
+});
+
+router.get('/edit/:id', async (req,res) => {
+    const { id } = req.params;
+    const task = await Task.findById({ _id: id});
+    res.render('edit', {
+        task
+    });
+});
+
+router.post('/edit/:id', async (req,res) => {
+    const { id } = req.params;
+    await Task.update({ _id: id }, req.body);
+    res.redirect('/');
+});
 module.exports = router;
