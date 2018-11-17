3e99f5184112d6a194704ebc55ca4d74dfa912d6
Patricio urbieta <patricio.urbieta@gmail.com>
2018-11-17 19:46:36 -0300

    react babel

--- src/app/app.js
+++ src/app/app.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 
 class App extends Component {
-
-    render(){
-        return(
-            <h1>hola react</h1>
-        )
-    }
-
+  render() {
+    return (
+        <h1>hola</h1>
+    )
+  }
 }
+
+export default App;
